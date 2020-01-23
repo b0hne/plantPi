3fa5960dd87382e18111a9168ef79086ea40212a
Sebo <sebastian.benkel@tum.de>
2020-01-23 03:20:46 +0100

    reworks the output format

--- plant_bot.py
+++ plant_bot.py
@@ -1,7 +1,6 @@
 import time
 import telepot as pot
 import serial
-
 from telepot.loop import MessageLoop
 
 """
@@ -13,20 +12,16 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=200)
 
 def handle(msg):
     chat_id = msg['chat']['id']
-    
     bot.sendMessage(chat_id, lines)
-    print(lines)
 
-bot = pot.Bot('<bot-token>')
 pot.loop.MessageLoop(bot, handle).run_as_thread()
-print('I am listening ...')
-time.sleep(2)
+
 i = 0
 while 1:
-    time.sleep(2)    
-    print(str(ser.readline())[2:-5])
-    lines[i] = str(ser.readline())[2:5]
+    time.sleep(50)
+    lines[i] = str(ser.readline())[2:-5]
 
-    if float(((lines[i])[:6])) < 60:
+    if float((lines[i][0:5])) < 60:
+        print(float((lines[i][0:5])))
         bot.sendMessage(433881991, lines[i])
     i = (i+1)%3
