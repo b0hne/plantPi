0a2a25d43a111c8a179346280ba2cff41dc86553
Sebo <sebastian.benkel@tum.de>
2020-01-23 14:15:03 +0100

    changes to simultanious readout every 150 seconds

--- plant_bot.py
+++ plant_bot.py
@@ -17,12 +17,12 @@ def handle(msg):
 bot = pot.Bot('<bot id>')
 pot.loop.MessageLoop(bot, handle).run_as_thread()
 
-i = 0
 while 1:
-    time.sleep(50)
-    lines[i] = str(ser.readline())[2:-5]
+    for i in range(3):
+        lines[i] = str(ser.readline())[2:-5]
 
-    if float((lines[i][0:5])) < 60:
-        print(float((lines[i][0:5])))
-        bot.sendMessage(433881991, lines[i])
-    i = (i+1)%3
+        if float((lines[i][0:5])) < 60:
+            print(float((lines[i][0:5])))
+            bot.sendMessage(433881991, lines[i])
+        i = (i+1)%3
+    time.sleep(150)
