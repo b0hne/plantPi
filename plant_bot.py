327c7214c5f811788a0c48996d181618d79569c7
b0hne <s.benkel@tum.de>
2020-02-22 00:20:44 +0100

    adds alive message

--- plant_bot.py
+++ plant_bot.py
@@ -6,23 +6,31 @@ from telepot.loop import MessageLoop
 """
 pybot for telegram control
 """
-lines = ['']*3
+num_plants = 5
+lines = ['']*num_plants
+telegram_id = 'users telegramid'
+
+def check():
+    for i in range(num_plants):
+        lines[i] = str(ser.readline())[2:-5]
+        if float((lines[i][0:5])) < 45:
+            bot.sendMessage(telegram_id, lines[i])
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=200)
-
+bot = pot.Bot('<bot-id>')
 def handle(msg):
     chat_id = msg['chat']['id']
+    if msg == 'recheck':
+        check()
     bot.sendMessage(chat_id, lines)
 
-bot = pot.Bot('<bot id>')
+
 pot.loop.MessageLoop(bot, handle).run_as_thread()
-
+timer = 0
 while 1:
-    for i in range(3):
-        lines[i] = str(ser.readline())[2:-5]
-
-        if float((lines[i][0:5])) < 60:
-            print(float((lines[i][0:5])))
-            bot.sendMessage(433881991, lines[i])
-        i = (i+1)%3
-    time.sleep(150)
+    if timer == 0:
+        bot.sendMessage(telegram_id, 'I am still alife')
+    timer += 1
+    timer %= 48
+    check()
+    time.sleep(1800)
